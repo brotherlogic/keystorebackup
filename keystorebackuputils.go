f0c9d9355a1b8e9bf34d2052094d98255a43550d
Simon Tucker <simon@brotherlogic@gmail>
2019-10-02 11:32:31 -0700

    Better errors.

--- keystorebackuputils.go
+++ keystorebackuputils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"golang.org/x/net/context"
 
@@ -22,7 +23,7 @@ func (s *Server) syncKeys(ctx context.Context) error {
 func (s *Server) performSync(ctx context.Context) error {
 	resp, err := s.keystore.getDirectory(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error getting directory: %v", err)
 	}
 
 	for _, key := range resp.Keys {
@@ -50,15 +51,22 @@ func (s *Server) performSync(ctx context.Context) error {
 	return nil
 }
 
+func trim(key string) string {
+	if strings.HasPrefix(key, "/") {
+		return key[1:]
+	}
+	return key
+}
+
 func (s *Server) saveData(ctx context.Context, index int, key *pbks.FileMeta) error {
 	resp, err := s.keystore.read(ctx, &pbks.ReadRequest{Key: key.Key})
 	if err != nil {
-		return err
+		return fmt.Errorf("Error on read: %v", err)
 	}
 
 	pl := resp.Payload
 	data, _ := proto.Marshal(pl)
-	err = ioutil.WriteFile(s.saveDirectory+"/"+fmt.Sprintf("%v.backup-%v", key.Key, key.Version), data, 0644)
+	err = ioutil.WriteFile(s.saveDirectory+fmt.Sprintf("%v.backup-%v", trim(key.Key), key.Version), data, 0644)
 	if err == nil {
 		s.saves++
 		if index >= 0 {
